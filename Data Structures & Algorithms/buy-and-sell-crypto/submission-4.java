b41c43db0b62313c70f8fbb78049617c336b279e
Precious Michael <eyiza.mich@gmail.com>
2026-04-20 14:20:58 +0100

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for(int i = 1;i < prices.length; i++){
+            if(prices[i] > min){
+                int profit = prices[i] - min;
+                if (profit > maxProfit) maxProfit = profit;
+            } else {
+                min = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
